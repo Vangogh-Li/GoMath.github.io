096ede2782cfaedb2f35b733258818219e20d21a
Vangogh-Li <linwangli2022@gmail.com>
2024-06-08 15:04:46 -0400

    Add files via upload

---
+++ GoMath.js
@@ -0,0 +1,16 @@
+const pageNumber = (total, max, current) =>{
+   
+}
+
+function PaginationButtons (totalPages, maxPageVisible = 1, currentPage = 1){
+let pages = PaginationButtons (totalPages, maxPageVisible, currentPage);
+let currentPageBtn = null;
+
+const PaginationButtons = document.createElement('div');
+pagination.className = 'pagination-buttons';
+
+this.render = (container = document.body)=>{
+    container.appendChild(PaginationButtonsContainer)
+}
+
+}
